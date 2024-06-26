b4737047023cb930dc6d9b2658198a4955e52d4d
slash-arkh <slash-arkh@mail.ru>
2024-06-26 06:48:35 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -1,10 +1,10 @@
 class House:
     def __init__(self, numberOfFloors):
-        self.numberOfFloors = numberOfFloors
+        self.numberOfFloors = 0
 
     def setNewNumberOfFloors(self, floors):
         self.numberOfFloors = floors
         print(self.numberOfFloors)
 
-zel = House(2)
+zel = House(0)
 zel.setNewNumberOfFloors(8)
